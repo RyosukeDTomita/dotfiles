dd873c24d0847015652061fbf91516259308bc8a
RyosukeDTomita <d310151c2@gmail.com>
2025-04-05 22:26:11 +0900

    chore: fix error

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
+#/bin/bash
 # NOTE: The environment variable `REMOTE_CONTAINERS` is true in Dev Containers.
 if [ "${REMOTE_CONTAINERS}" != "true" ]; then
   exit
 fi
 
 for f in $(ls -a ~/dotfiles | grep "^\.[a-zA-Z0-9]"); do
-  if [ "${f}" == ".git" ] || [ "${f}" == ".config" ]; then
+  if [ "${f}" = ".git" ] || [ "${f}" = ".config" ] || [ "${f}" = ".ssh" ]; then
     continue
   fi
-  if [ ! -e ~/${f} ]; then
-    cp ~/dotfiles/${f} ~/${f}
-  fi
+  cp ~/dotfiles/${f} ~/${f}
 done
